6ca772a3722bc556578e4fc41cc57f43d18f3756
Scubaboy <robin@robincawsey.plus.com>
2015-12-12 19:37:31 +0000

    Added test method to hub

--- QuadSignalRServer/Hubs/ActiveQuads/ActiveQuadsHub.cs
+++ QuadSignalRServer/Hubs/ActiveQuads/ActiveQuadsHub.cs
@@ -2,11 +2,21 @@
 {
     using Microsoft.AspNet.SignalR;
     using Microsoft.AspNet.SignalR.Hubs;
+    using QuadModels;
     using System.Threading.Tasks;
 
     [HubName("ActiveQuadsHub")]
     public class ActiveQuadsHub : Hub
     {
+        public void Test()
+        {
+            var im = "w";
+        }
+
+        public void UpdateQuad(ActiveQuad activeQuad)
+        {
+            var quad = activeQuad;
+        }
         public override Task OnConnected()
         {
             return base.OnConnected();
